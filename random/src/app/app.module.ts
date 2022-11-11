5b6bf5916b588413121e6d8cb8a4db12dbc47130
Aadi9910 <informarcialaditya@gmail.com>
2022-11-11 10:49:50 +0530

    ok

--- random/src/app/app.module.ts
+++ random/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadersComponent } from './components/headers/headers.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadersComponent,
+    ButtonComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
