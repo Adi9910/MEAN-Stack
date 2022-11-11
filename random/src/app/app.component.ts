51ed3f2de7bf7718152a5449d77ab191c54b0aea
Aadi9910 <informarcialaditya@gmail.com>
2022-11-11 13:35:54 +0530

    no css

--- random/src/app/app.component.ts
+++ random/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<div class="container">
+    <app-headers></app-headers>
+    <app-tasks></app-tasks>
+  </div>`,
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
- 
-}
+export class AppComponent {}
