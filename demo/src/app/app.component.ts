ec5d038f413f8f891ac1f8cf1d5811e0a9ef3b0e
Aadi9910 <informarcialaditya@gmail.com>
2022-11-11 14:06:09 +0530

    pure

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div class="container">
+  template: `<div class="container">MAGIC TABLE
     <app-headers></app-headers>
     <app-tasks></app-tasks>
   </div>`
