a6b737e8d23f189e1075adef8d5cfc28dd886d87
Aadi9910 <informarcialaditya@gmail.com>
2022-11-11 20:39:34 +0530

    good

--- demo/src/app/components/headers/headers.component.ts
+++ demo/src/app/components/headers/headers.component.ts
@@ -3,12 +3,48 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-headers',
   template: `<header>
-  <h1>{{ title }} <app-button color="blue" text="ADD" (btnClick)="toggleAddTask()"></app-button></h1>
-</header>
-`
+      <h1>
+        {{ title }}
+        <app-button
+          color="blue"
+          text="ADD"
+          (btnClick)="toggleAddTask()"
+        ></app-button>
+      </h1>
+    </header>
+    <body>
+      <div style="width:100%;">
+        <div style="width:40%;float:left;border:solid 1px black;">
+          <div>{{ 6589.23 | currency: 'INR' }}</div>
+          <div>{{ 6589.23 | currency: 'USD':true }}</div>
+          <div>{{ todaydate | date: 'd/MMM/yy,hh/mm' }}</div>
+          <div>{{ todaydate | date: 'shortTime' }}</div>
+          <div>{{ 454.78787814 | number: '3.4' }}</div>
+          <div>{{ jsonval | json }}</div>
+          <div>{{ 00.54565 | percent }}</div>
+          <div>{{ months | slice: 0:4 }}</div>
+        </div>
+      </div>
+    </body> `,
 })
 export class HeadersComponent implements OnInit {
-  title = 'aditya';
+  title = 'my-first-app';
+  todaydate = new Date();
+  jsonval = { name: 'Alex', age: '25' };
+  months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'April',
+    'May',
+    'Jun',
+    'July',
+    'Aug',
+    'Sept',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
   constructor() {}
 
   ngOnInit(): void {}
