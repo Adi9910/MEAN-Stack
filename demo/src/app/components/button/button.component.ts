f6060590dbe831c5bf1a26d4a3ef6de722abb80f
Aadi9910 <informarcialaditya@gmail.com>
2022-11-12 07:58:01 +0530

    fresh

--- demo/src/app/components/button/button.component.ts
+++ demo/src/app/components/button/button.component.ts
@@ -1,20 +1,21 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  template: `<button class="btn" [ngStyle]="{ 'background-color': color }" (click)="onClick()">
+  template: `<button 
+  class="btn" 
+  [ngStyle]="{ 'background-color': color }" 
+  (click)="onClick()">
   {{ text }}
 </button>
 `
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent {
   @Input() text: string = '';
   @Input() color: string = '';
   @Output() btnClick = new EventEmitter();
 
   constructor() {}
-
-  ngOnInit(): void {}
 
   onClick() {
     this.btnClick.emit();
--- demo/src/app/components/headers/headers.component.ts
+++ demo/src/app/components/headers/headers.component.ts
@@ -13,10 +13,10 @@ import { Component, OnInit } from '@angular/core';
       </h1>
     </header>
     <body>
-      <div style="width:100%;">
+      <div style="width:100%;" *ngIf="isVisible">
         <div style="width:40%;float:left;border:solid 1px black;">
           <div>{{ 6589.23 | currency: 'INR' }}</div>
-          <div>{{ 6589.23 | currency: 'USD':true }}</div>
+          <div>{{ 6589.23 | currency: 'USD':false }}</div>
           <div>{{ todaydate | date: 'd/MMM/yy,hh/mm' }}</div>
           <div>{{ todaydate | date: 'shortTime' }}</div>
           <div>{{ 454.78787814 | number: '3.4' }}</div>
@@ -30,26 +30,15 @@ import { Component, OnInit } from '@angular/core';
 export class HeadersComponent implements OnInit {
   title = 'my-first-app';
   todaydate = new Date();
-  jsonval = { name: 'Alex', age: '25' };
-  months = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'April',
-    'May',
-    'Jun',
-    'July',
-    'Aug',
-    'Sept',
-    'Oct',
-    'Nov',
-    'Dec',
-  ];
+  isVisible : Boolean = false
+  jsonval = { name: 'Aditya', age: '22' };
+  months = ['Jan','Feb','Mar','April','May','Jun','July','Aug','Sept','Oct','Nov','Dec']
   constructor() {}
 
   ngOnInit(): void {}
 
   toggleAddTask() {
     console.log('toggle');
+    this.isVisible = !this.isVisible
   }
 }
