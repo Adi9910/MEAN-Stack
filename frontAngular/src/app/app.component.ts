c54d0f03ecdafe533cc2f572a4d4a46498d9302f
Aadi9910 <informarcialaditya@gmail.com>
2023-08-26 15:05:55 +0530

    1-commit

--- frontAngular/src/app/app.component.ts
+++ frontAngular/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/services/post.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontAngular';
+  Hidden = true;
+  name: string = '';
+  emp: number = 0;
+  posts: any;
+  constructor(private service: PostService) {}
+  group = [{ name: 'Aditya', emp: 23 }];
+  switch() {
+    this.Hidden = !this.Hidden;
+  }
+  add() {
+    this.group.push({
+      name: this.name,
+      emp: this.emp,
+    });
+    console.log('grouop', this.group);
+  }
+
+  ngOnInit() {
+    this.service.getPosts().subscribe((res) => {
+      this.posts = res;
+    });
+  }
 }
